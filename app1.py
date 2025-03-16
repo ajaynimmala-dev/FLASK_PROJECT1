506a37956d08a0cc5f3142adc2d6e9d7ef69b540
Ajay <ajaynimmala18@gmail.com>
2025-03-16 12:29:05 +0530

    home page changed

--- app1.py
+++ app1.py
@@ -1,12 +1,22 @@
 from flask import Flask,render_template,request
 app=Flask(__name__)
+
+data=[{
+    'name':'ganesh',
+    'regdno':'y23cs001',
+    'branch':'CSE'
+},{
+    'name':'ramesh',
+    'regdno':'y23cs002',
+    'branch':'AIML'
+}]
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=data,title='abcd')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='abcd')
 
 
 @app.route('/get',methods=['POST','GET'])
