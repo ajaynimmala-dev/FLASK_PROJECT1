0dcc6c0d557a6bceef599282e55b9a19b6ef1c43
Ajay <ajaynimmala18@gmail.com>
2025-03-20 21:32:49 +0530

    routing user registration

--- app1.py
+++ app1.py
@@ -1,16 +1,20 @@
 from flask import Flask,render_template,request,url_for
+
+from forms import RegistrationForm,LoginForm
 app=Flask(__name__)
 
+app.config['SECRET_KEY']='e58ca8c71f5d9e5150acd9af436cc9f1'
+
 data=[{
-    'name':'GANESH',
-    'regdno':'Y23CS001',
-    'branch':'CSE',
-    'year':'2ND YEAR'
+    'author':'Corey Schafer',
+    'title':'Blog Post 1',
+    'content':'First post content',
+    'date_posted':'April 20,2018'
 },{
-    'name':'RAMESH',
-    'regdno':'Y23CS002',
-    'branch':'AIML',
-    'year':'2ND YEAR'
+    'author':'Jane Doe',
+    'title':'Blog Post 2',
+    'content':'Second post content',
+    'date_posted':'April 21,2018'
 }]
 @app.route('/')
 def home():
@@ -25,6 +29,16 @@ def about():
 def getData():
     temp=request.form
     return 'success'
+
+@app.route('/register',methods=['POST','GET'])
+def register():
+    form=RegistrationForm()
+    return render_template('Register.html',title='Register',form=form)
+
+@app.route('/login')
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='Login',form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
