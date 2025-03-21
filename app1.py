373d2952cfbf807888146caacdc1e21e29b7ebec
Ajay <ajaynimmala18@gmail.com>
2025-03-21 18:27:44 +0530

    updateing login page

--- app1.py
+++ app1.py
@@ -38,7 +38,7 @@ def register():
 @app.route('/login')
 def login():
     form=LoginForm()
-    return render_template('login.html',title='Login',form=form)
+    return render_template('Login.html',title='Login',form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
