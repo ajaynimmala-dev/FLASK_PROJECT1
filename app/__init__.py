28887c8d752b34686d9aefd18d70f05cd2393425
Ajay <ajaynimmala18@gmail.com>
2025-07-06 20:48:20 +0530

    User Registration update

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app=Flask(__name__)
 
@@ -8,5 +8,6 @@ app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'e58ca8c71f5d9e5150acd9af436cc9f1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from app import routes
