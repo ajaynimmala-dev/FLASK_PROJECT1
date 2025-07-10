13be064d8de76613f131ceec0e15c777d181cc4a
Ajay <ajaynimmala18@gmail.com>
2025-07-10 20:36:03 +0530

    Created Account Route

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 from app.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
@@ -32,3 +33,22 @@ class LoginForm(FlaskForm):
     remember=BooleanField('Saved password')
 
     submit=SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators = [DataRequired(),Length(min=2,max=20)])
+
+    email = StringField('Email',validators = [DataRequired(),Email()])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError('That username is already exists!')
+
+    def validate_email(self,email):
+        if email.data != current_user.username:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('That email is already exists!')
