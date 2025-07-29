cfa453f2966aba06bdd4aaa7c6511f00ec3ed706
Ajay <ajaynimmala18@gmail.com>
2025-07-29 21:01:57 +0530

    Handling Errors

--- app/config.py
+++ app/config.py
@@ -1,11 +1,14 @@
 import os
+import json
 
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or  'dev_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///site.db'
+    SECRET_KEY = config.get('SECRET_KEY') or  'dev_secret_key'
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'ajaykumarnimmala18@gmail.com'
-    MAIL_PASSWORD = 'hrwmusceilshcxqr'
+    MAIL_USERNAME = config.get('EMAIL_USER')
+    MAIL_PASSWORD = config.get('EMAIL_PASS')
