209c97698e253bf4d2f34f277a93fe8ed4594651
Ajay <ajaynimmala18@gmail.com>
2025-09-14 08:47:29 +0530

    added photos in home page

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ if os.path.exists('/etc/config.json'):
 
 class Config:
     SECRET_KEY = config.get('SECRET_KEY') or  'dev_secret_key'
-    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
